0af91f363f07bbcf543607c18754e9fcf66c99c3
danielschweigert <dan.schweigert@gmail.com>
2017-10-02 18:27:47 -0700

    script to start spark stream processing

---
+++ 02-Spark/start_stream_processing.sh
@@ -0,0 +1,3 @@
+# starts pyspark batch streaming
+
+/usr/local/spark/bin/spark-submit --master spark://ip-10-0-0-7:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 02-spark/streaming_calculations.py
